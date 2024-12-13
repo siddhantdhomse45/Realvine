b209113015c3ef73f1eaeef8748ef87b35fbd8da
siddhantdhomse45 <siddh654@gmail.com>
2024-12-13 17:37:59 -0500

    third commit

--- src/page/property/PropertyDetail.jsx
+++ src/page/property/PropertyDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import propertyData from '../../DAta';
+import propertyData from '../../Data';
 import './property-detail.css';
 
 function PropertyDetail() {
