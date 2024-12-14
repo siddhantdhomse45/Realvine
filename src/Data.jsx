c82b888fcebdc0cb4add222cfe99fa6a110e1357
siddhantdhomse45 <siddh654@gmail.com>
2024-12-13 21:16:30 -0500

    third commit

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,3 +1,4 @@
+
 const propertyData = [
     {
       id: 1,
--- src/page/property/PropertyDetail.jsx
+++ src/page/property/PropertyDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import propertyData from './Data';
+import propertyData from "./page/property/PropertyDetail.jsx"
 import './property-detail.css';
 
 function PropertyDetail() {
